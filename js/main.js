62322288e1d5553ae95deefd9716f36080777e06
mostafaelhawary1987 <hoohoo1987@gmail.com>
2026-01-17 08:05:40 +0200

    finish V-arbic

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(window).on("load", function () {
+  $("body").removeClass("overflow");
+});
+$(document).ready(function () {
+
+  $("#openNav").click( function() {
+
+    $('.sidenav').addClass('open');
+    $('.overlay').fadeIn();
+    $('body').addClass('ovh');
+  });
+  
+  $(".closebtn,.overlay").click( function() {
+    $('.sidenav').removeClass('open');
+    $('.overlay').fadeOut();
+    $('body').removeClass('ovh');
+  });
+  // Product Counting
+  $("button.plus").click(function(e) {
+    e.preventDefault();
+    var newValue = parseFloat(
+      $(this)
+        .parent()
+        .find(".quantity")
+        .val()
+    );
+    $(this)
+      .parent()
+      .find(".quantity")
+      .val(newValue + 1);
+  });
+
+  $("button.minus").click(function(e) {
+    e.preventDefault();
+    var newValue = parseFloat(
+      $(this)
+        .parent()
+        .find(".quantity")
+        .val()
+    );
+    if (newValue <= 1) {
+    } else {
+      $(this)
+        .parent()
+        .find(".quantity")
+        .val(newValue - 1);
+    }
+  });
+
+});
